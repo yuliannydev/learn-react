2adf052d2bcf8fee84aa83de7956ff710b78efbf
Yulianny Betancourt <flipsdulce@gmail.com>
2022-10-24 20:41:55 -0500

    :test_tube: Init testing in Counter App react

--- 02-counter-app-vite/src/FirstApp.jsx
+++ 02-counter-app-vite/src/FirstApp.jsx
@@ -1,5 +1,21 @@
-export default function FirstApp({content}) {
+import PropTypes from 'prop-types';
+export default function FirstApp({title, subTitle, name}) {
   return (
-    <p> {content} </p>
+    <>
+    <h1> {title} </h1>
+    <p> {subTitle} </p>
+    <em> {name} </em>
+    </>
   )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+}
+
+FirstApp.defaultProps = {
+  name: 'Nathalie Rivas',
+  title: 'Hola poshito',
+  subTitle: 'Desde tu mente :3'
 }
--- 02-counter-app-vite/src/main.jsx
+++ 02-counter-app-vite/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.css'
-import { CounterApp } from './CounterApp';
-// import FirstApp from './FirstApp';
+// import { CounterApp } from './CounterApp';
+import FirstApp from './FirstApp';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <CounterApp value={0}/>
-      {/* <FirstApp content = 'Hello world!'/> */}
+        {/* <CounterApp value={0}/> */}
+      <FirstApp title = 'Hello world!' subTitle = 'Desde React' name = 'Yulianny Betancourt'/>
     </React.StrictMode>
 )
