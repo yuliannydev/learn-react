e4cb7bfabac0db9299716b9f39e6b8f70655991e
Yulianny Betancourt <flipsdulce@gmail.com>
2022-12-08 23:13:12 -0500

    :test_tube: Finish test in component GifItem

--- 03-gif-expert-app/tests/components/GifItem.test.jsx
+++ 03-gif-expert-app/tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { GifItem } from '../../src/components/GifItem'
 
 describe('GifItem', () => { 
@@ -7,7 +7,34 @@ describe('GifItem', () => {
     const image_url = 'https://hunter-x-hunter.com/h-x-h.webp';
 
     test('Snapshot Title and image_url required', () => { 
-     const { container } = render( <GifItem title={title} image_url={image_url} /> )   
-     expect(container).toMatchSnapshot();
+      const { container } = render( <GifItem title={title} image_url={image_url} /> )   
+      expect(container).toMatchSnapshot();
+    })
+
+    test('The image must have the url image', () => { 
+        
+      render( <GifItem title={title} image_url={image_url} /> )   
+      //screen.debug();
+      /** Desestrcturando el src de la imagen */
+      const { src } = screen.getByRole('img');
+      //console.log(image);
+      expect(src).toBe(image_url);
+    })
+
+    test('The image must have text ALT', () => {
+
+      render( <GifItem title={title} image_url={image_url} /> )   
+      /**Concatenando a la img el alt para ser especifico */
+      const textAlt = screen.getByRole('img').alt;
+      expect(textAlt).toBe(title);
+    })
+
+    test('The component must have a text', () => {
+    
+      render(<GifItem title={title} image_url={image_url} />)
+      expect(screen.getByText(title)).toBeTruthy();
+      /** En los test anteriores el title era un atributo de una
+       * property, no un texto renderizado en el screen */
      })
+
  })
